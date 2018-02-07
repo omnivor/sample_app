2244c45de9dab4ce692715b433d86b8a38808384
omnivor <lukasz.targanski@gmail.com>
2018-02-07 14:27:41 +0100

    Add account activation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180201171426) do
+ActiveRecord::Schema.define(version: 20180207123010) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20180201171426) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.string "activated_at"
+    t.string "datetime"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
