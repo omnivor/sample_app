3ba0c7a7b695414258873da75d9f2c7792fab554
omnivor <lukasz.targanski@gmail.com>
2018-03-20 15:00:19 +0100

    Add user following

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -71,4 +71,33 @@ class UserTest < ActiveSupport::TestCase
       		@user.destroy
     	end
   	end
+
+  	test "should follow and unfollow a user" do
+    	marlena = users(:marlena)
+    	lukasz  = users(:lukasz)
+    	assert_not marlena.following?(lukasz)
+    	marlena.follow(lukasz)
+    	assert marlena.following?(lukasz)
+    	assert lukasz.followers.include?(marlena)
+    	marlena.unfollow(lukasz)
+    	assert_not marlena.following?(lukasz)
+  	end
+
+  	test "feed should have the right posts" do
+    michael = users(:marlena)
+    archer  = users(:lukasz)
+    lana    = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
